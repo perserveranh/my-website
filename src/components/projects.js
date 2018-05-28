7116c3c913f2061b393faac41b05cd728257e9bc
tuananh <tuan.anh.257.it@gmail.com>
2018-05-28 23:28:25 +0700

    Caoursel

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Tabs,Tab , Grid , Cell, Card , CardTitle, CardText , CardActions, CardMenu, Button , IconButton } from 'react-mdl';
+import Slider from "react-slick";
+import Carousel from 'nuka-carousel';
 
 class Project extends Component {
 	constructor(props) {
@@ -12,7 +14,8 @@ class Project extends Component {
 	if(this.state.activeTab === 0)
 		{
 		return(
-			<div className="projects-grid">
+			// <div className="projects-grid">
+			<Cell col={12} className="projects-grid">
 			<Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
 				<CardTitle style={{color: '#0e0412', height: '176px',background:'url(http://indigo.amsterdam/wp-content/uploads/2017/06/React-icon.svg_-e1498055414258.png) center/cover '}}>React</CardTitle>
 				<CardText>
@@ -28,7 +31,7 @@ class Project extends Component {
 					<IconButton name="share" />
 				</CardMenu>
 			</Card>
-							<Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+			<Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
 				<CardTitle style={{color: '#0e0412', height: '176px',background:'url(http://indigo.amsterdam/wp-content/uploads/2017/06/React-icon.svg_-e1498055414258.png) center/cover '}}>React</CardTitle>
 				<CardText>
 					 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -43,7 +46,7 @@ class Project extends Component {
 					<IconButton name="share" />
 				</CardMenu>
 			</Card>
-						<Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+			<Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
 				<CardTitle style={{color: '#0e0412', height: '176px',background:'url(http://indigo.amsterdam/wp-content/uploads/2017/06/React-icon.svg_-e1498055414258.png) center/cover '}}>React</CardTitle>
 				<CardText>
 					 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -58,28 +61,124 @@ class Project extends Component {
 					<IconButton name="share" />
 				</CardMenu>
 			</Card>
-			</div>
+			</Cell>
+			// </div>
 			)
 		}
 	else if(this.state.activeTab === 1){
 		return(
-			<div><h1>This is Angular</h1></div>
+			<Grid className="demo-grid-2">
+				<Cell col={3}><Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+					<CardTitle style={{color: '#0e0412', height: '280px',background:'url(http://startupsac.com/wp-content/uploads/2015/10/AngularJS-Logo.png) center/cover '}}>Angular</CardTitle>
+						<CardText>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Mauris sagittis pellentesque lacus eleifend lacinia...
+						</CardText>
+					<CardActions border >
+						<Button colored>Github</Button>
+						<Button colored>CodePen</Button>
+						<Button colored>LiveDemo</Button>
+					</CardActions>
+					<CardMenu style={{color: '#55056f'}}>
+						<IconButton name="share" />
+					</CardMenu>
+				</Card></Cell>
+				<Cell col={3}><Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+				<CardTitle style={{color: '#0e0412', height: '280px',background:'url(http://startupsac.com/wp-content/uploads/2015/10/AngularJS-Logo.png) center/cover '}}>Angular</CardTitle>
+				<CardText>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Mauris sagittis pellentesque lacus eleifend lacinia...
+				</CardText>
+				<CardActions border >
+				<Button colored>Github</Button>
+				<Button colored>CodePen</Button>
+				<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: '#55056f'}}>
+				<IconButton name="share" />
+				</CardMenu>
+				</Card></Cell>
+				<Cell col={3}><Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+				<CardTitle style={{color: '#0e0412', height: '280px',background:'url(http://startupsac.com/wp-content/uploads/2015/10/AngularJS-Logo.png) center/cover '}}>Angular</CardTitle>
+				<CardText>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Mauris sagittis pellentesque lacus eleifend lacinia...
+				</CardText>
+				<CardActions border >
+				<Button colored>Github</Button>
+				<Button colored>CodePen</Button>
+				<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: '#55056f'}}>
+				<IconButton name="share" />
+				</CardMenu>
+				</Card></Cell>
+				<Cell col={3}><Card shadow={5} style={{minWidth: '500' ,margin: 'auto'}}>
+				<CardTitle style={{color: '#0e0412', height: '280px',background:'url(http://startupsac.com/wp-content/uploads/2015/10/AngularJS-Logo.png) center/cover '}}>Angular</CardTitle>
+				<CardText>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Mauris sagittis pellentesque lacus eleifend lacinia...
+				</CardText>
+				<CardActions border >
+				<Button colored>Github</Button>
+				<Button colored>CodePen</Button>
+				<Button colored>LiveDemo</Button>
+				</CardActions>
+				<CardMenu style={{color: '#55056f'}}>
+				<IconButton name="share" />
+				</CardMenu>
+				</Card></Cell>
+			</Grid>
 			)
 	}
 	else if(this.state.activeTab === 2){
+			var settings = {
+      dots: true,
+      infinite: true,
+      speed: 10000,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 		return(
-			<div><h1>This is VueJS</h1></div>
+			<Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
 			)
 	}
 	else if(this.state.activeTab === 3){
 		return(
-			<div><h1>This is MongoDB</h1></div>
+			<Carousel>
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+        <img style={{width:'100%',height:'400px'}} alt='xxx' src="https://www.cloudsigma.com/wp-content/uploads/mongodb-cloud-servers-862x287.png" />
+      </Carousel>
 			)
 	}
 
 
 	}
 	render() {
+	
 		return(
 			<div className="category-tabs">
 				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
@@ -90,9 +189,9 @@ class Project extends Component {
 				</Tabs>
 				
 				<Grid >
-					<Cell col={12}>
-					<div className="content">{this.toogleCategogies()}</div>
-					</Cell>
+				
+					{this.toogleCategogies()}
+					
 				</Grid>
 			</div>
 			)
